c3751ae533921380a84cf78b232a12e05382679e
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-11-25 20:15:18 +0200

    Add initial prep script

---
+++ scripts/prepare.js
@@ -0,0 +1,21 @@
+/*
+.env
+  year=2022
+
+take year from env
+check date
+if dates year matches from 1-24 days
+if no match ask to manually enter the date yyyy-mm-dd
+
+check if folder exists for the date
+if no folder
+part 1
+else part 2
+prefill date and folder, and ask to confirm
+
+
+
+
+ */
+
+console.log('test');
