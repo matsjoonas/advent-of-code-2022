258cd6667b3ff6c5880a3222d84de031238f2566
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-11-30 22:17:38 +0200

    Add initial logic for folder creation

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -14,6 +14,17 @@ const argv = yargs(hideBin(process.argv)).argv;
 const inform = (message) => console.log(chalk.cyan(message));
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
+function createGetPath(dayFolderName) {
+  function getPath(subPath) {
+    let segment = `../${dayFolderName}`;
+    if (subPath) {
+      segment += `/${subPath}`;
+    }
+    return path.resolve(__dirname, segment);
+  }
+  return getPath;
+}
+
 async function main() {
   let date;
   const dateValidator = new DateValidator();
@@ -30,15 +41,24 @@ async function main() {
 
   inform('\nStarting to prep for date: ' + date.toDateString());
 
-  let day = date.getDate().toString();
-  if (day.length === 1) {
-    day = '0' + day;
+  let dayString = date.getDate().toString();
+  if (dayString.length === 1) {
+    dayString = '0' + dayString;
   }
 
-  const fullPath = path.resolve(__dirname, `../day${day}`);
-  if (fs.existsSync(fullPath)) {
-    throw new Error(`Directory already exists: ${fullPath}`);
+  const dayFolderName = 'day' + dayString;
+  const getPath = createGetPath(dayFolderName);
+  const fullDayPath = path.resolve(__dirname, `../${dayFolderName}`);
+  if (fs.existsSync(getPath())) {
+    throw new Error(`Directory already exists: ${getPath()}`);
   }
+
+  inform('\nCreating day root folder');
+  fs.mkdirSync(getPath());
+  inform('\nCreating tests folder');
+  fs.mkdirSync(getPath('tests'));
+  inform('\nCreating input file');
+  fs.writeFileSync(getPath(`input${dayString}.txt`), '')
 }
 
 main().catch((err) => {
