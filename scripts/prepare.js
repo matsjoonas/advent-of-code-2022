15eaf4d08421789305d00d8418b765e144124382
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-11-30 21:55:58 +0200

    Add existing directory check

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -1,12 +1,18 @@
 import yargs from 'yargs';
 import got from 'got';
 import {hideBin} from "yargs/helpers";
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
+import chalk from 'chalk';
+import path from 'path';
+import * as url from 'url';
 dotenv.config({
   path: 'scripts/.env',
 })
 import DateValidator from './DateValidator.js';
+import fs from 'fs';
 const argv = yargs(hideBin(process.argv)).argv;
+const inform = (message) => console.log(chalk.cyan(message));
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
 async function main() {
   let date;
@@ -22,7 +28,17 @@ async function main() {
     throw new Error(validationResult.message);
   }
 
+  inform('\nStarting to prep for date: ' + date.toDateString());
 
+  let day = date.getDate().toString();
+  if (day.length === 1) {
+    day = '0' + day;
+  }
+
+  const fullPath = path.resolve(__dirname, `../day${day}`);
+  if (fs.existsSync(fullPath)) {
+    throw new Error(`Directory already exists: ${fullPath}`);
+  }
 }
 
 main().catch((err) => {
