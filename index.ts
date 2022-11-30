20a7240823ec3d299404fa380ca0b5f24a6597dc
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-11-30 18:42:43 +0200

    Move solvers to a separate file

--- index.ts
+++ index.ts
@@ -1,16 +1,7 @@
 import yargs from 'yargs/yargs';
 import {hideBin} from "yargs/helpers";
-import Day01Part1 from './Day01/Day01Part1';
-import Day01Part2 from "./Day01/Day01Part2";
 import AocSuite from "./AocSuite";
-
-// should probs be a separate file for easier generation
-const solvers = [
-  [
-    new Day01Part1(),
-    new Day01Part2(),
-  ]
-]
+import solvers from "./solvers";
 
 const suite = new AocSuite(solvers);
 
---
+++ solvers.ts
@@ -0,0 +1,12 @@
+import Day from "./Day";
+import Day01Part1 from "./Day01/Day01Part1";
+import Day01Part2 from "./Day01/Day01Part2";
+
+const solvers: Day[][] = [
+  [
+    new Day01Part1(),
+    new Day01Part2(),
+  ]
+];
+
+export default solvers;
