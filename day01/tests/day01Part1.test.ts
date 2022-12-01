81dc2fe5db48c58ac356539d524fc576064fc3c6
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-12-01 09:27:41 +0200

    Add day 1 solutions

--- day01/tests/day01Part1.test.ts
+++ day01/tests/day01Part1.test.ts
@@ -4,7 +4,11 @@ import Day01Part1 from "../Day01Part1";
 const dataSets = [
   {
     inputPath: './day01/tests/day01TestInput1.txt',
-    expectedResult: '',
+    expectedResult: 24000,
+  },
+  {
+    inputPath: './day01/tests/day01TestInput2.txt',
+    expectedResult: 72718,
   },
 ]
 
