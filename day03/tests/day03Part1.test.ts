3f806773248edb6c3b97940f8a6ab99104fcb3a6
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-12-02 09:59:48 +0200

    Add boilerplate for day03

---
+++ day03/tests/day03Part1.test.ts
@@ -0,0 +1,12 @@
+import dayTest from "../../dayTest";
+import Day03Part1 from "../Day03Part1";
+
+const dataSets = [
+  {
+    inputPath: './day03/tests/day03TestInput1.txt',
+    expectedResult: '',
+  },
+]
+
+dayTest(new Day03Part1(), dataSets);
+
---
+++ day03/tests/day03Part2.test.ts
@@ -0,0 +1,12 @@
+import dayTest from "../../dayTest";
+import Day03Part2 from "../Day03Part2";
+
+const dataSets = [
+  {
+    inputPath: './day03/tests/day03TestInput1.txt',
+    expectedResult: '',
+  },
+]
+
+dayTest(new Day03Part2(), dataSets);
+
