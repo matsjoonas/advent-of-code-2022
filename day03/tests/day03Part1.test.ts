cebc1db2bdc9a1e3727dd9cefd3b97aa28533bf0
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-12-03 10:48:29 +0200

    chore: add datasests for day3 tests

--- day03/tests/day03Part1.test.ts
+++ day03/tests/day03Part1.test.ts
@@ -6,6 +6,10 @@ const dataSets = [
     inputPath: './day03/tests/day03TestInput1.txt',
     expectedResult: 157,
   },
+  {
+    inputPath: './day03/tests/day03TestInput2.txt',
+    expectedResult: 7917,
+  },
 ]
 
 dayTest(new Day03Part1(), dataSets);
--- day03/tests/day03Part2.test.ts
+++ day03/tests/day03Part2.test.ts
@@ -6,6 +6,10 @@ const dataSets = [
     inputPath: './day03/tests/day03TestInput1.txt',
     expectedResult: 70,
   },
+  {
+    inputPath: './day03/tests/day03TestInput2.txt',
+    expectedResult: 2585,
+  },
 ]
 
 dayTest(new Day03Part2(), dataSets);
