cc8649a1beca518d77e2f20f8a04ac0b01b89691
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-12-04 09:55:51 +0200

    Delete unneeded classes

--- day04/intersectionOfStrings.ts
+++
@@ -1,5 +0,0 @@
-import intersectionOfArrays from "./intersectionOfArrays";
-
-export default function intersectionOfStrings(a: string, b: string) {
-  return intersectionOfArrays(a.split(''), b.split('')).join();
-}
