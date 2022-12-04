d88838319d5523ccdf25d7b690007f28d18e0120
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-12-04 12:06:55 +0200

    Refactor

--- day04/Day04Part1.ts
+++ day04/Day04Part1.ts
@@ -9,19 +9,13 @@ export default class Day04Part1 implements Day {
           .map(range => range.split('-').map(Number));
       });
 
-    return input.reduce((acc, line) => {
-      let long = line[0];
-      let short = line[1];
-      if ((long[1] - long[0]) < (short[1] - short[0])) {
-        long = line[1];
-        short = line[0];
-      }
-
-      if (long[0] <= short[0] && long[1] >= short[1]) {
+    return input.reduce((acc, ranges) => {
+      const [a, b] = ranges;
+      if ((a[0] <= b[0] && a[1] >= b[1]) || (a[0] >= b[0] && a[1] <= b[1])) {
         return acc + 1;
       }
 
       return acc;
-    }, 0)
+    }, 0);
   }
 }
--- day04/Day04Part2.ts
+++ day04/Day04Part2.ts
@@ -9,30 +9,17 @@ export default class Day04Part2 implements Day {
           .map(range => range.split('-').map(Number));
       });
 
-    return input.reduce((acc, line) => {
-      let long = line[0];
-      let short = line[1];
-      if ((long[1] - long[0]) < (short[1] - short[0])) {
-        long = line[1];
-        short = line[0];
-      }
-
-      if (long[0] <= short[0] && long[1] >= short[1]) {
+    return input.reduce((acc, ranges) => {
+      const [a, b] = ranges;
+      if ((a[0] <= b[0] && a[1] >= b[1]) || (a[0] >= b[0] && a[1] <= b[1])) {
         return acc + 1;
       }
 
-      let early = line[0];
-      let late = line[1];
-      if (early[0] > late[0]) {
-        late = line[0];
-        early = line[1];
-      }
-      const startDiff = late[0] - early[0];
-      if (startDiff >= 0 && startDiff <= early[1] - early[0]) {
+      if (a[1] >= b[0] && a[0] <= b[1]) {
         return acc + 1;
       }
 
       return acc;
-    }, 0)
+    }, 0);
   }
 }
