32b7d600e9bffb6f01cfb8b9b9f6024402118cc5
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-12-08 09:03:49 +0200

    Add solutions to day08

--- day08/tests/day08Part1.test.ts
+++ day08/tests/day08Part1.test.ts
@@ -6,6 +6,10 @@ const dataSets = [
     inputPath: './day08/tests/day08TestInput1.txt',
     expectedResult: 21,
   },
+  {
+    inputPath: './day08/tests/day08TestInput2.txt',
+    expectedResult: 1812,
+  },
 ]
 
 dayTest(new Day08Part1(), dataSets);
--- day08/tests/day08Part2.test.ts
+++ day08/tests/day08Part2.test.ts
@@ -4,7 +4,11 @@ import Day08Part2 from "../Day08Part2";
 const dataSets = [
   {
     inputPath: './day08/tests/day08TestInput1.txt',
-    expectedResult: '',
+    expectedResult: 8,
+  },
+  {
+    inputPath: './day08/tests/day08TestInput2.txt',
+    expectedResult: 315495,
   },
 ]
 
