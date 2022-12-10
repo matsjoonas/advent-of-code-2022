e2a8260d8970a3ef2eb5abb5bce798cbfba547fb
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-12-10 09:16:38 +0200

    Solve day10 part1

--- day10/Day10Part1.ts
+++ day10/Day10Part1.ts
@@ -1,8 +1,39 @@
 import Day from "../Day";
 
+/*
+For now, consider the signal strength
+(the cycle number multiplied by the value of the X register)
+during the 20th cycle and every 40 cycles after that
+(that is, during the 20th, 60th, 100th, 140th, 180th, and 220th cycles).
+ */
+
+function maybeSignalStrength(cycle: number, register: number, signalStrengths: number[]) {
+  if (cycle === 20 || ((cycle + 20) % 40 === 0)) {
+    signalStrengths.push(cycle * register);
+  }
+}
+
 export default class Day10Part1 implements Day {
   public solve(rawInput: string): number {
-    const input = rawInput.trim();
-    return 0;
+    const input = rawInput.trim().split(/\r\n|\n/);
+
+    let cycle = 1;
+    let registerX = 1;
+    const signalStrengths: number[] = [];
+    for (let i = 0; i < input.length; i++) {
+      const instr = input[i].split(' ');
+      if (instr[0] === 'noop') {
+        maybeSignalStrength(cycle, registerX, signalStrengths);
+        cycle ++;
+      } else {
+        maybeSignalStrength(cycle, registerX, signalStrengths);
+        cycle++;
+        maybeSignalStrength(cycle, registerX, signalStrengths);
+        cycle++;
+        registerX += Number(instr[1]);
+      }
+    }
+
+    return signalStrengths.reduce((acc, cur) => acc + cur, 0);
   }
 }
