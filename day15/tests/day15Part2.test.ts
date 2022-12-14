482e4c6dd89b3ecb82406c9b7585caa2b778aebf
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-12-14 09:35:55 +0200

    Prep for day15

---
+++ day15/tests/day15Part2.test.ts
@@ -0,0 +1,12 @@
+import dayTest from "../../dayTest";
+import Day15Part2 from "../Day15Part2";
+
+const dataSets = [
+  {
+    inputPath: './day15/tests/day15TestInput1.txt',
+    expectedResult: '',
+  },
+]
+
+dayTest(new Day15Part2(), dataSets);
+
