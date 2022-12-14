714842e25785eed3db2aa5b3d8c9ff22857ca7ff
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-12-14 09:34:07 +0200

    feat: add solutions to day14 (#16)

---
+++ day14/tests/day14Part1.test.ts
@@ -0,0 +1,12 @@
+import dayTest from "../../dayTest";
+import Day14Part1 from "../Day14Part1";
+
+const dataSets = [
+  {
+    inputPath: './day14/tests/day14TestInput1.txt',
+    expectedResult: 24,
+  },
+]
+
+dayTest(new Day14Part1(), dataSets);
+
---
+++ day14/tests/day14Part2.test.ts
@@ -0,0 +1,12 @@
+import dayTest from "../../dayTest";
+import Day14Part2 from "../Day14Part2";
+
+const dataSets = [
+  {
+    inputPath: './day14/tests/day14TestInput1.txt',
+    expectedResult: 93,
+  },
+]
+
+dayTest(new Day14Part2(), dataSets);
+
