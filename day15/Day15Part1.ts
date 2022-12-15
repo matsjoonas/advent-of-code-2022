e8921103e91e4e2953c04adbc222f2a3f228de66
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-12-15 08:01:15 +0200

    Parse teh input

--- day15/Day15Part1.ts
+++ day15/Day15Part1.ts
@@ -2,7 +2,18 @@ import Day from "../Day";
 
 export default class Day15Part1 implements Day {
   public solve(rawInput: string): number {
-    const input = rawInput.trim();
+    const input = rawInput.trim().split(/\r\n|\n/).map(line => {
+      return line.replace('Sensor at x=', '')
+        .replace(': closest beacon is at x=', '|')
+        .replaceAll('y=', '')
+        .split('|')
+        .map(item => item.split(', ').map(Number));
+    });
+    console.log(input);
+
+    // sort and calc the grid size
+    // calc the tacicab dist to beacon, so, Sensor, beacon, taxicab
+    // for each point -> for each sensor get the distance from point to sensor, if out of range of all, then count that
     return 0;
   }
 }
