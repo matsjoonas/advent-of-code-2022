829e971477f00477fa0d2ae5db008f798d303d1d
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-12-17 09:30:12 +0200

    Prep for day17

---
+++ day17/Day17Part1.ts
@@ -0,0 +1,9 @@
+import Day from "../Day";
+
+export default class Day17Part1 implements Day {
+  public solve(rawInput: string): number {
+    const input = rawInput.trim();
+    console.log(input);
+    return 0;
+  }
+}
---
+++ day17/tests/day17Part1.test.ts
@@ -0,0 +1,12 @@
+import dayTest from "../../dayTest";
+import Day17Part1 from "../Day17Part1";
+
+const dataSets = [
+  {
+    inputPath: './day17/tests/day17TestInput1.txt',
+    expectedResult: '',
+  },
+]
+
+dayTest(new Day17Part1(), dataSets);
+
