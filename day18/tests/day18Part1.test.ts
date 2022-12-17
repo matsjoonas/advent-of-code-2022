802332493e1fd3de33e142201cf7a559e51a1baf
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-12-17 19:37:10 +0200

    Prep for day18

---
+++ day18/tests/day18Part1.test.ts
@@ -0,0 +1,12 @@
+import dayTest from "../../dayTest";
+import Day18Part1 from "../Day18Part1";
+
+const dataSets = [
+  {
+    inputPath: './day18/tests/day18TestInput1.txt',
+    expectedResult: '',
+  },
+]
+
+dayTest(new Day18Part1(), dataSets);
+
---
+++ day18/tests/day18Part2.test.ts
@@ -0,0 +1,12 @@
+import dayTest from "../../dayTest";
+import Day18Part2 from "../Day18Part2";
+
+const dataSets = [
+  {
+    inputPath: './day18/tests/day18TestInput1.txt',
+    expectedResult: '',
+  },
+]
+
+dayTest(new Day18Part2(), dataSets);
+
