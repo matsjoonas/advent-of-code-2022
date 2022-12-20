b20798b55391dcf9ede456d6de04be6c8ce84045
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-12-20 09:28:11 +0200

    Update tests

--- day20/tests/day20Part1.test.ts
+++ day20/tests/day20Part1.test.ts
@@ -4,7 +4,7 @@ import Day20Part1 from "../Day20Part1";
 const dataSets = [
   {
     inputPath: './day20/tests/day20TestInput1.txt',
-    expectedResult: '',
+    expectedResult: 3,
   },
 ]
 
--- day20/tests/day20Part2.test.ts
+++ day20/tests/day20Part2.test.ts
@@ -4,7 +4,7 @@ import Day20Part2 from "../Day20Part2";
 const dataSets = [
   {
     inputPath: './day20/tests/day20TestInput1.txt',
-    expectedResult: '',
+    expectedResult: 1623178306,
   },
 ]
 
