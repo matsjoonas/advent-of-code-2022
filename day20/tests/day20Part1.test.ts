955497628e68ab08f76c28f4c3ccaae22f023aae
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-12-20 09:32:20 +0200

    feat: add solutions to day20 (#22)

---
+++ day20/tests/day20Part1.test.ts
@@ -0,0 +1,12 @@
+import dayTest from "../../dayTest";
+import Day20Part1 from "../Day20Part1";
+
+const dataSets = [
+  {
+    inputPath: './day20/tests/day20TestInput1.txt',
+    expectedResult: 3,
+  },
+]
+
+dayTest(new Day20Part1(), dataSets);
+
---
+++ day20/tests/day20Part2.test.ts
@@ -0,0 +1,12 @@
+import dayTest from "../../dayTest";
+import Day20Part2 from "../Day20Part2";
+
+const dataSets = [
+  {
+    inputPath: './day20/tests/day20TestInput1.txt',
+    expectedResult: 1623178306,
+  },
+]
+
+dayTest(new Day20Part2(), dataSets);
+
