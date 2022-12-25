52ff81cb47ef93966de7a0c11aca47d941c69d21
Mats-Joonas Kulla <matsjoonas@gmail.com>
2022-12-25 08:47:16 +0200

    tweaks

--- day25/Day25Part1.ts
+++ day25/Day25Part1.ts
@@ -1,8 +1,29 @@
 import Day from "../Day";
 
+function snafuToBase10(snafuString: string) {
+  const snafuArr = snafuString.split('').map(item => {
+    return Number(item.replace('-', '-1').replace('=', '-2'));
+  });
+  snafuArr.reverse();
+  let base10 = 0;
+  snafuArr.forEach((snafu, index) => {
+    base10 += Math.pow(5, index) * snafu;
+  });
+
+  return base10;
+}
+
+function base10ToSnafu(base10: number) {
+  console.log(base10.toString(5));
+  return parseInt(base10.toString(), 5);
+}
+
 export default class Day25Part1 implements Day {
   public solve(rawInput: string): number {
-    const input = rawInput.trim();
-    return 0;
+    const input = rawInput.trim().split(/\r\n|\n/);
+
+    const result = input.reduce((acc, cur) => acc + snafuToBase10(cur), 0);
+
+    return base10ToSnafu(result);
   }
 }
